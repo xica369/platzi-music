fb138b292afd347d5aa1b7b3b1c1f760be587535
xica369 <761@holbertonschool.com>
2019-11-16 18:14:50 -0500

    basic concepts vue.js

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   #app
     img(src="./assets/logo.png")
     h1 {{ msg }}
+    p(v-show="showValue") {{ value }}
+    ul
+      li(v-for="i in mensaje") {{ i }}
+    input(v-model="year")
+    p {{ age }}
+    input(v-model="name")
+    button(v-on:click="format") Format
+    p {{ formattedName }}
 </template>
 
 <script>
@@ -9,9 +17,27 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'hola Diego'
+      name: "",
+      formattedName: "",
+      msg: 'hola Diego',
+      value: 'algo',
+      showValue: true,
+      year: "",
+      mensaje: ["besame", "la", "boquita"]
+    }
+  },
+  computed: {
+    age () {
+      return 2019 - parseInt(this.year)
+    }
+  },
+
+  methods: {
+    format () {
+      this.formattedName = this.name.split(" ").join("-")
     }
   }
+
 }
 </script>
 
